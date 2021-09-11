fd8cf0534d3d8764749927955e7a44dcb83b9769
M.Inzamam Malik <malikasinger1@gmail.com>
2021-09-11 23:38:23 +0500

    use ref and dependency array

--- React/01.hello-world/src/index.js
+++ React/01.hello-world/src/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 function Hi() {
-  return <div>Hello World!</div>;
+  return <div>
+    
+    
+    Hello World!
+    
+    <p> <h1>sdsd</h1> </p>
+    
+    </div>;
 }
 
 ReactDOM.render(<Hi/>, document.querySelector('#root'));
