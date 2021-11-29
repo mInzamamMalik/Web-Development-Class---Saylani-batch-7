13481ccc3bd0dd8b52441969344448bf5acdc185
M.Inzamam Malik <malikasinger1@gmail.com>
2021-11-29 16:10:57 +0500

    remaining code

--- 06 Native/helloWorld/App.js
+++ 06 Native/helloWorld/App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world!</Text>
+      <Text>Hello world! my app 123</Text>
       <StatusBar style="auto" />
     </View>
   );
